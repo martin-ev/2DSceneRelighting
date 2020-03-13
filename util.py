ba800c7d079ab2dc28c184b8ede84d59f229bd05
Bzdeco <jakub.gwizdala@gmail.com>
2020-03-13 14:04:21 +0100

    Template notebook with tensorboard setup

---
+++ util.py
@@ -0,0 +1,16 @@
+import pwd
+import os
+from datetime import datetime
+
+def generate_run_name(label=None):
+    '''
+    Generates name for the run monitored on tensorboard.
+    Optional label can contain additional description to identify the run easier.
+    '''
+    time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    owner = pwd.getpwuid(os.getuid()).pw_name  # get login of the person who ran the script
+    if label is None:
+        return f'{time}_{owner}'
+    else:
+        return f'{time}_{owner}_{label}'
+    

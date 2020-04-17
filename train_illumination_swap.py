5a180b9f84be9f288eaacb1dd0b041dd06eb09ac
Jakub Gwizdala <jakub.gwizdala@gmail.com>
2020-04-17 18:24:02 +0200

    Add verbosity about visualization and dataset, order visualized images

--- train_illumination_swap.py
+++ train_illumination_swap.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -17,7 +16,7 @@ device = setup_device(GPU_IDS)
 
 # Parameters
 NAME = 'illumination_swap_only_abandonned_6500'
-BATCH_SIZE = 10
+BATCH_SIZE = 25
 NUM_WORKERS = 4
 EPOCHS = 50
 
@@ -35,12 +34,16 @@ dataset = dataset.DifferentTargetSceneDataset(locations=['scene_abandonned_city_
                                               target_colors=['6500'],
                                               transform=Resize(256))
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+DATASET_SIZE = len(dataset)
+print(f'Dataset contains {DATASET_SIZE} samples.')
+print(f'Running with batch size: {BATCH_SIZE} for {EPOCHS} epochs.')
 
 # Configure tensorboard
 writer = tensorboard.setup_summary_writer(NAME)
 tensorboard_process = tensorboard.start_tensorboard_process()
 SHOWN_SAMPLES = 3
-VISUALIZATION_FREQ = 5000  # every how many batches tensorboard is updated with new images
+VISUALIZATION_FREQ = DATASET_SIZE // 4  # every how many batches tensorboard is updated with new images
+print(f'{SHOWN_SAMPLES} samples will be visualized every {VISUALIZATION_FREQ} batches.')
 
 # Train loop
 for epoch in range(1, EPOCHS+1):
@@ -68,23 +71,24 @@ for epoch in range(1, EPOCHS+1):
 
         # Visualize current progress
         if batch_idx % VISUALIZATION_FREQ == 0:
-            writer.add_image('Visualization/Input', make_grid(x[:SHOWN_SAMPLES]), epoch)
-            writer.add_image('Visualization/Relighted', make_grid(relighted_image[:SHOWN_SAMPLES]), epoch)
-            writer.add_image('Visualization/Ground-truth', make_grid(ground_truth[:SHOWN_SAMPLES]), epoch)
-            writer.add_image('Visualization/Target', make_grid(target[:SHOWN_SAMPLES]), epoch)
+            writer.add_image('Visualization/1-Input', make_grid(x[:SHOWN_SAMPLES]), epoch)
+            writer.add_image('Visualization/2-Relighted', make_grid(relighted_image[:SHOWN_SAMPLES]), epoch)
+            writer.add_image('Visualization/3-Ground-truth', make_grid(ground_truth[:SHOWN_SAMPLES]), epoch)
+            writer.add_image('Visualization/4-Target', make_grid(target[:SHOWN_SAMPLES]), epoch)
 
-            writer.add_image('Env-map/Relighted', make_grid(relighted_env_map[:SHOWN_SAMPLES].view(-1, 3, 16, 32)), epoch)
-            writer.add_image('Env-map/Ground-truth', make_grid(gt_env_map[:SHOWN_SAMPLES].view(-1, 3, 16, 32)), epoch)
+            writer.add_image('Env-map/1-Relighted', make_grid(relighted_env_map[:SHOWN_SAMPLES].view(-1, 3, 16, 32)),
+                             epoch)
+            writer.add_image('Env-map/2-Ground-truth', make_grid(gt_env_map[:SHOWN_SAMPLES].view(-1, 3, 16, 32)), epoch)
 
     # Evaluate
     model.eval()
     # TODO: Add test set evaluation here
 
     # Update tensorboard training losses
-    writer.add_scalar('Loss/Total training loss', train_loss, epoch)
-    writer.add_scalars('Loss/Training loss components', {
-        'Reconstruction': train_loss_reconstruction,
-        'Env map': train_loss_env_map
+    writer.add_scalar('Loss/1-Total', train_loss, epoch)
+    writer.add_scalars('Loss/2-Components', {
+        '1-Reconstruction': train_loss_reconstruction,
+        '2-Env-map': train_loss_env_map
     }, epoch)
 
 # Store trained model

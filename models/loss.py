8df26d3f5ade7b5067ef9c339b062ba3705f7543
Jakub Gwizdala <jakub.gwizdala@gmail.com>
2020-04-17 21:27:29 +0200

    Fix log loss

--- models/loss.py
+++ models/loss.py
@@ -4,5 +4,5 @@ import torch
 def log_l2_loss(x, target):
     # there is an issue with torch.norm (https://github.com/pytorch/pytorch/issues/30704) that's why it's done this way
     b, c, h, w = x.size()
-    n = float(B*C*H*W)  # total number of elements
+    n = float(b*c*h*w)  # total number of elements
     return (x.log1p_() - target.log1p_()).pow(2).sum() / n

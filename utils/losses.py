7616df923694269d825a5933acbe58f896059546
martin-ev <55559086+martin-ev@users.noreply.github.com>
2020-04-20 16:36:24 +0200

    minor

--- utils/losses.py
+++ utils/losses.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from math import log
 
 def log_l2_loss(x, target):
     # there is an issue with torch.norm (https://github.com/pytorch/pytorch/issues/30704) that's why it's done this way
